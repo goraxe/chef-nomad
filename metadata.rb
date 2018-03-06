894dfa17fecc6eb8bcedf8345d9d3d50d7ea7b18
Gordon Irving <goraxe@gmail.com>
2018-03-05 22:29:43 -0800

    bump dep

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures nomad'
 long_description 'Installs/Configures nomad'
 version          '0.1.1'
 
-depends 'service_manager', '~> 0.1.0'
+depends 'service_manager', '~> 0.2.0'
